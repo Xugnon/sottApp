2cd0f8574f30a9b8ea608bc73d87460297ed030a
Lucas <85856491+Xugnon@users.noreply.github.com>
2023-11-03 15:10:26 -0300

    feat: create routes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 mongoose
+  //Connection to database
   .connect('mongodb://localhost:27017')
   .then(() => {
+    //On successful database connection, run the API
     const app = express();
 
     app.listen(3001, () => {
