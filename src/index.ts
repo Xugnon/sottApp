d16669bd08b0d1a885d94ad4af4d7d9ce462378c
Lucas <85856491+Xugnon@users.noreply.github.com>
2023-11-04 08:50:56 -0300

    feat: create routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './routes';
+
 mongoose
   //Connection to database
   .connect('mongodb://localhost:27017')
@@ -8,8 +10,10 @@ mongoose
     //On successful database connection, run the API
     const app = express();
 
+    app.use(router);
+
     app.listen(3001, () => {
       console.log('🚀 Server is running on http://localhost:3001');
     });
   })
-  .catch(() => console.log('Erro ao conectar ao mongo'));
+  .catch(() => console.log('Error connecting to mongo'));
